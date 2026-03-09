26ef55fdca99661c391ff9d434d00ccb26759389
Gonzalo Flores Escobar <gonzalo.flores@getjusto.com>
2026-03-09 00:33:34 -0300

    Use social icons in share bar

--- components/ShareBar.tsx
+++ components/ShareBar.tsx
@@ -49,31 +49,43 @@ export function ShareBar({ url, title, labels }: ShareBarProps) {
       <div className="small" style={{ marginBottom: 8 }}>{labels.title}</div>
       <div className="share-bar">
         <a
-          className="share-button"
+          className="share-button icon-only"
           href={`https://x.com/intent/tweet?text=${encodedText}&url=${encodedUrl}`}
           target="_blank"
           rel="noreferrer"
           onClick={() => track('x')}
+          aria-label="X"
+          title="X"
         >
-          X
+          <svg className="share-icon" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill="currentColor" d="M18.244 2H21l-6.52 7.45L22 22h-6.8l-5.33-6.98L4.3 22H2l7.05-8.06L2 2h6.92l4.82 6.33L18.244 2zm-1.19 18h1.88L7.01 4h-2.0l12.044 16z"/>
+          </svg>
         </a>
         <a
-          className="share-button"
+          className="share-button icon-only"
           href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
           target="_blank"
           rel="noreferrer"
           onClick={() => track('facebook')}
+          aria-label="Facebook"
+          title="Facebook"
         >
-          Facebook
+          <svg className="share-icon" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill="currentColor" d="M22 12a10 10 0 1 0-11.5 9.9v-7H8v-3h2.5V9.3c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.4h-1.3c-1.3 0-1.7.8-1.7 1.6V12H16l-.4 3h-2.7v7A10 10 0 0 0 22 12z"/>
+          </svg>
         </a>
         <a
-          className="share-button"
+          className="share-button icon-only"
           href={`https://wa.me/?text=${encodedText}%20${encodedUrl}`}
           target="_blank"
           rel="noreferrer"
           onClick={() => track('whatsapp')}
+          aria-label="WhatsApp"
+          title="WhatsApp"
         >
-          WhatsApp
+          <svg className="share-icon" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill="currentColor" d="M20 12a8 8 0 0 0-12.7-6.4A8 8 0 0 0 6 20l-1.1 4L9 22.9A8 8 0 0 0 20 12zm-8 6.6a6.5 6.5 0 0 1-3.3-.9l-.2-.1-2 .5.5-1.9-.1-.2a6.5 6.5 0 1 1 5.1 2.6zm3.7-4.9c-.2-.1-1.3-.6-1.5-.7-.2-.1-.3-.1-.4.1-.1.2-.5.7-.6.8-.1.1-.2.2-.4.1a5.3 5.3 0 0 1-1.6-1 6 6 0 0 1-1.1-1.4c-.1-.2 0-.3.1-.4.1-.1.2-.2.3-.4.1-.1.1-.2.2-.3.1-.1.1-.2 0-.4-.1-.1-.4-1-.6-1.3-.2-.4-.3-.3-.4-.3h-.4c-.1 0-.4.1-.6.3-.2.2-.8.7-.8 1.8 0 1 .8 2.1 1 2.2.1.1 1.6 2.4 3.9 3.4.6.3 1.1.4 1.4.5.6.2 1.2.2 1.6.1.5-.1 1.3-.5 1.5-1 .2-.5.2-1 .1-1.1-.1-.1-.2-.2-.4-.3z"/>
+          </svg>
         </a>
         <button className="share-button" type="button" onClick={handleCopy}>
           {copied ? labels.copied : labels.copy}
